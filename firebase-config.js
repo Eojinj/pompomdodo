b60e69e7f3735aa4b264219cd52dc8a874845315
dabinishere <zefeni@knu.ac.kr>
2025-12-07 22:53:07 +0900

    firebase 실시간 구축 및 사용자 마지막 설정 저장

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.firebaseConfig = {
+    apiKey: "AIzaSyCEUB6mcFH_MN0W5cnFQjCbSzU1irJBPLg",
+    authDomain: "pompomdodo-ff55b.firebaseapp.com",
+    databaseURL: "https://pompomdodo-ff55b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pompomdodo-ff55b",
+    storageBucket: "pompomdodo-ff55b.firebasestorage.app",
+    messagingSenderId: "589657806472",
+    appId: "1:589657806472:web:f33db87042ba2d6880713d",
+    measurementId: "G-1DBG0KERKJ"
+  };
+  
